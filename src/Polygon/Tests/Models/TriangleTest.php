f8e39f1aec32a71377248785e1bac978c978a3a1
PatriciaFM <patriciafm4@gmail.com>
2016-12-27 21:16:18 +0100

    Triangle testcase modifed after failure

--- src/Polygon/Tests/Models/TriangleTest.php
+++ src/Polygon/Tests/Models/TriangleTest.php
@@ -1,12 +1,33 @@
 <?php
+declare(strict_types = 1);
+
+use Polygon\Models\Triangle;
 
 /**
- * Created by PhpStorm.
- * User: patri
- * Date: 27-12-16
- * Time: 20:46
+ * Class TriangleTest
+ *
+ * Testcase for Striangule class
  */
-class TriangleTest
+class TriangleTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * Test that Perimeter Method Should Return Triangle Perimeter
+     */
+    public function testPerimeterMethodShouldReturnTrianglePerimeter()
+    {
+        $triangle = new Triangle(10, 20, 25, 25);
+
+        $this->assertEquals(60, $triangle->perimeter());
+    }
+
+    /**
+     * Test that Area Method Should Return Triangle Area
+     */
+    public function testAreaMethodShouldReturnTriangleArea()
+    {
+        $triangle = new Triangle(10, 20, 25, 25);
+
+        $this->assertEquals(100, $triangle->area());
+    }
 
 }
